2112aec792ee1fada43f26c0ce93afa6ad2f9c27
kalunge <muthomititus+github@gmail.com>
2022-06-06 19:46:43 +0300

    Done with functions lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (distance) => {
+  return Math.abs(distance - 42);
+};
+
+const distanceFromHqInFeet = (distance) => {
+  return Math.abs(distance - 42) * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  return Math.abs(destination - start) * 264;
+};
+
+const calculatesFarePrice = (start, destination) => {
+  const distanceInFeet = Math.abs(destination - start) * 264;
+  if (distanceInFeet <= 264) {
+    return 0;
+  } else if (distanceInFeet >= 400 && distanceInFeet <= 2000) {
+    return 0.02 * (distanceInFeet - 400);
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else if (distanceInFeet > 2500) {
+    return "cannot travel that far";
+  }
+};
